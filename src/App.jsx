9beeea812eb48c3fc6dd5c5e243b6fc21ca7a140
MohamedamaD <mohamed.amad24483@gmail.com>
2026-07-03 15:40:36 +0300

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+import { Link, Route, Routes } from "react-router-dom";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <Navbar bg="light" data-bs-theme="light" expand="md">
+        <Container>
+          <Navbar.Brand>Py React</Navbar.Brand>
+
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav>
+              <Nav.Item>
+                <Nav.Link as={Link} to="/">
+                  Home
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Container className="py-4">
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="*" element={<h1>Not Found Page 404</h1>} />
+        </Routes>
+      </Container>
+    </>
   );
 }
